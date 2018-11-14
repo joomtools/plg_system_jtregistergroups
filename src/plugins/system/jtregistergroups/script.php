ab135fc9a6605401e806538a10b0fbc106b3f574
Guido De Gobbis <guido@degobbis.de>
2018-11-14 21:57:37 +0100

    Installer-Script hinzugefügt

---
+++ src/plugins/system/jtregistergroups/script.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package      Joomla.Plugin
+ * @subpackage   System.Jtregistergroups
+ *
+ * @author       Guido De Gobbis <support@joomtools.de>
+ * @copyright    (c) 2018 JoomTools.de - All rights reserved.
+ * @license      GNU General Public License version 3 or later
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Script file of Joomla CMS
+ *
+ * @since  1.0.0
+ */
+class PlgSystemJtregistergroupsInstallerScript
+{
+	/**
+	 * Extension script constructor.
+	 *
+	 * @since   1.0.0
+	 */
+	public function __construct()
+	{
+		// Define the minumum versions to be supported.
+		$this->minimumJoomla = '3.8';
+		$this->minimumPhp    = '7.1';
+	}
+
+	/**
+	 * Function to act prior to installation process begins
+	 *
+	 * @param   string      $action     Which action is happening (install|uninstall|discover_install|update)
+	 * @param   JInstaller  $installer  The class calling this method
+	 *
+	 * @return   boolean  True on success
+	 * @since    1.0.0
+	 */
+	public function preflight($action, $installer)
+	{
+		$app = JFactory::getApplication();
+		JFactory::getLanguage()->load('plg_content_jteasylaw', dirname(__FILE__));
+
+		if (version_compare(PHP_VERSION, $this->minimumPhp, 'lt'))
+		{
+			$app->enqueueMessage(JText::_('PLG_SYSTEM_JTREGISTERGROUPS_MINPHPVERSION'), 'error');
+
+			return false;
+		}
+
+		if (version_compare(JVERSION, $this->minimumJoomla, 'lt'))
+		{
+			$app->enqueueMessage(JText::_('PLG_SYSTEM_JTREGISTERGROUPS_MINJVERSION'), 'error');
+
+			return false;
+		}
+
+		return true;
+	}
+}
